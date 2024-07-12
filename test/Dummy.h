b6032504f3863efc3354e351b36ec010d7a6e402
Martin Hierholzer <martin.hierholzer@desy.de>
2024-07-12 17:47:21 +0200

    fix(test): remove unused variable

--- test/Dummy.h
+++ test/Dummy.h
@@ -98,14 +98,12 @@ struct Dummy<bool> : public ChimeraTK::ApplicationModule {
     // The first string will be initalized to "" instead of "0", but we don't care here
     writeAll();
     // Set to 1 so that the second entry will be "1"
-    int i = 1;
     out = false;
     while(true) {
       trigger.read();
       out = !out;
       outTrigger = (int)trigger;
       writeAll();
-      ++i;
     }
   }
 };
