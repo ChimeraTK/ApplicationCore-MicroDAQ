f37fbbd030e53df4e741acedbd72bec117110c83
Martin Hierholzer <martin.hierholzer@desy.de>
2025-03-10 16:42:55 +0100

    fix: code formatting

--- test/Dummy.h
+++ test/Dummy.h
@@ -22,7 +22,7 @@ typedef boost::mpl::list<int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, 
 struct DummyWithTag : public ChimeraTK::ApplicationModule {
   DummyWithTag(ChimeraTK::ModuleGroup* module, const std::string& name, const std::string& description,
       const std::string& tag = "DAQ")
-  : ChimeraTK::ApplicationModule(module, name, description), out(this, "out", "", "Dummy output", {tag}){};
+  : ChimeraTK::ApplicationModule(module, name, description), out(this, "out", "", "Dummy output", {tag}) {};
 
   ChimeraTK::ScalarOutput<int> out;
   ChimeraTK::ScalarOutput<int> outTrigger{this, "outTrigger", "", "Dummy output"};
